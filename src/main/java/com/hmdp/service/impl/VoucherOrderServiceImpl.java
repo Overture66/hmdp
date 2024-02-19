eb56340b86648115fd84df8ad0c75e71300aa117
Overture <chaoccc2022@163.com>
2024-02-19 17:11:22 +0800

    优惠券秒杀实现

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
+import com.hmdp.service.IVoucherService;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +29,50 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private RedisIdWorker redisIdWorker;
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            return Result.fail("秒杀未开始");
+        }
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("秒杀已经结束");
+        }
+        if (voucher.getStock()<1) {
+            return Result.fail("库存不足");
+        }
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()) {
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    @Transactional
+    public  Result createVoucherOrder(Long voucherId) {
+        Long userId = UserHolder.getUser().getId();
+            Long count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+            if (count > 0) {
+                return Result.fail("用户已经抢购过一次");
+            }
+            boolean success = seckillVoucherService.update()
+                    .setSql("stock=stock-1")
+                    .eq("voucher_Id", voucherId)
+                    .gt("stock", 0).update();
+            if (!success) {
+                return Result.fail("库存不足");
+            }
+            VoucherOrder voucherOrder = new VoucherOrder();
+            Long order = redisIdWorker.nextId("order");
+            voucherOrder.setId(order);
+            voucherOrder.setUserId(userId);
+            voucherOrder.setVoucherId(voucherId);
+            save(voucherOrder);
+            return Result.ok(order);
+    }
 }
---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,33 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+    private static final long BEGIN_TIMESTAMP=1704067200L;
+    private static final int COUNT_BITS=32;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public Long nextId(String keyPrefix){
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timeStamp = nowSecond - BEGIN_TIMESTAMP;
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        Long count = stringRedisTemplate.opsForValue().increment("icr" + keyPrefix + ":" + date);
+
+        return timeStamp<<COUNT_BITS | count;
+    }
+
+
+}
