eb5b27290136e949b94db370afa0e9d8c451cd47
Overture <chaoccc2022@163.com>
2024-02-22 17:01:33 +0800

    用户关注实现

--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -17,4 +31,52 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private IUserService userService;
+    @Override
+    public Result follow(Long followUserid, boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        String key = "follow:" + userId;
+        if (isFollow) {
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserid);
+            boolean isSuccess = save(follow);
+            if (isSuccess) {
+                stringRedisTemplate.opsForSet().add(key,followUserid.toString());
+            }
+        } else {
+            boolean isSuccess = remove(new QueryWrapper<Follow>().eq("user_id", userId).eq("follow_user_id", followUserid));
+            if (isSuccess) {
+            stringRedisTemplate.opsForSet().remove(key,followUserid.toString());
+            }
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result isFollow(Long followUserid) {
+        Long userId = UserHolder.getUser().getId();
+        Long count = query().eq("user_id", userId).eq("follow_user_id", followUserid).count();
+        return Result.ok(count>0L);
+    }
+
+    @Override
+    public Result followCommons(Long id) {
+        Long userId = UserHolder.getUser().getId();
+        String key1 = "follow:" + userId;
+        String key2 = "follow:" + id;
+        Set<String> intersect = stringRedisTemplate.opsForSet().intersect(key1, key2);
+        if (intersect==null||intersect.isEmpty()) {
+            return Result.ok(Collections.emptyList());
+        }
+        List<Long> ids = intersect.stream().map(Long::valueOf).collect(Collectors.toList());
+        List<UserDTO> users = userService.listByIds(ids)
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(users);
+    }
 }
